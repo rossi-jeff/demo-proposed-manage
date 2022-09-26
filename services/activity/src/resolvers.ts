4f4b408f4dcc401c32d96828b10cecfa1d659b88
Jeff Rossi <Jeffrey.Rossi@snapraise.com>
2022-09-26 15:47:10 -0400

    add to resolvers

--- services/activity/src/resolvers.ts
+++ services/activity/src/resolvers.ts
@@ -1,13 +1,18 @@
 import type { Resolvers } from "../generated/graphql";
 import { Activity } from "./Activity/field-resolvers";
+import { Event } from "./Event/field-resolvers";
+import { events, event } from "./Event/query-resolvers";
 import { Group } from "./Group/field-resolvers";
 import { group, groups } from "./Group/query-resolvers";
 
 export const resolvers: Resolvers = {
   Query: {
+    events,
+    event,
     groups,
     group,
   },
   Activity,
+  Event,
   Group,
 };
